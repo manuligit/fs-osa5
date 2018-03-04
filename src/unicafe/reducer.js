4eb7b7f3368378337bd25ddc2a26facb0c57a81e
manuligit <riina.vaisanen@gmail.com>
2018-03-04 17:55:04 +0200

    t5.18

--- src/unicafe/reducer.js
+++ src/unicafe/reducer.js
@@ -7,18 +7,18 @@ const initialState = {
 }
 
 const counterReducer = (state = initialState, action) => {
-  let calculateAverage = () => {
-    if ((state.good - state.bad) / (state.good + state.bad + state.ok))  {
-      return ((state.good - state.bad) / (state.good + state.bad + state.ok)) 
+  let calculateAverage = (good, bad, ok) => {
+    if ((good - bad) / (good + bad + ok))  {
+      return ((good - bad) / (good + bad + ok)) 
     } else {
       //show 0 instead of NaN
       return 0
     } 
   }
 
-  let calculatePositive = () => {
-    if ( (state.good / (state.good + state.bad + state.ok))) {
-      return (state.good / (state.good + state.bad + state.ok)) 
+  let calculatePositive = (good, bad, ok) => {
+    if ( (good / (good + bad + ok))) {
+      return (good / (good + bad + ok)) 
     } else {
       //show 0 instead of NaN
       return 0
@@ -28,24 +28,22 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      let goodCounter = { ...state, good: state.good + 1, average: calculateAverage(), 
-                          positive: calculatePositive() }
+      let goodCounter = { ...state, good: state.good + 1, average: calculateAverage(state.good+1, state.bad, state.ok), 
+                          positive: calculatePositive(state.good+1, state.bad, state.ok) }
       return goodCounter
     case 'OK':
       let okCounter = { ...state, ok: state.ok + 1,
-                        average: calculateAverage(), positive: calculatePositive()  }
+                        average: calculateAverage(state.good, state.bad, state.ok+1), 
+                        positive: calculatePositive(state.good, state.bad, state.ok+1)  }
       return okCounter
     case 'BAD':
       let badCounter = { ...state, bad: state.bad + 1,
-                         average: calculateAverage(), positive: calculatePositive()  }
+                         average: calculateAverage(state.good, state.bad+1, state.ok), 
+                         positive: calculatePositive(state.good, state.bad+1, state.ok)  }
       return badCounter
     case 'ZERO':
       let zeroedCounter = { ...state, good: 0, ok: 0, bad: 0, average: 0, positive: 0 }
       return zeroedCounter
-    case 'AVERAGE':
-      return ((state.good - state.bad) / (state.good + state.bad + state.ok))
-    case 'POSITIVE':
-      return (state.good / (state.good + state.bad + state.ok))
   }
   return state
 }
